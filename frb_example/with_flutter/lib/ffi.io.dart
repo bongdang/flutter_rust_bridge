4769a7cfd5807ef301d1cee91480ce7f14497b91
bongdang <95209776+bongdang@users.noreply.github.com>
2023-04-20 16:24:00 +0900

    tiny fix for macos
    as you know, dynamic lib is dylib in macos. so, add it.

--- frb_example/with_flutter/lib/ffi.io.dart
+++ frb_example/with_flutter/lib/ffi.io.dart
@@ -3,6 +3,6 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
 import 'bridge_generated.io.dart';
 
 const base = 'flutter_rust_bridge_example';
-final path = Platform.isWindows ? '$base.dll' : 'lib$base.so';
+final path = Platform.isWindows ? '$base.dll' : (Platform.isLinux ? 'lib$base.so' : 'lib$base.dylib');
 late final dylib = loadLibForFlutter(path);
 late final api = FlutterRustBridgeExampleImpl(dylib);
